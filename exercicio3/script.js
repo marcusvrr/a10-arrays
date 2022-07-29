ad67c4074eb88c63691f61261bde48d554b2ff27
marcusvrr <marcusvrr2@gmail.com>
2022-07-28 22:28:02 -0300

    Exercícios de feedback corrigidos

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,6 +1,7 @@
 const lista1 = [87, 5,-3, 22,19]
 const lista2 = ['chocolate', 'pipoca', 'açai', 'nutella']
 const lista3 = [10, 'bola', false]
+//fazendo as copias
 const lista1Copia = lista1.slice();
 const lista2Copia = lista2.slice();
 const lista3Copia = lista3.slice();
@@ -16,7 +17,19 @@ console.log(lista2);
 console.log(lista2Copia);
 
 //parte3
-lista3Copia.splice(2,1);
+lista3Copia.splice(1,1);
 console.log(lista3);
 console.log(lista3Copia);
+/*"(i,n)"o indice do splice SEMPRE começa do elemento que quero excluir e o n = quantidade. confere?
+a primeira informação que a gente passa é o índice do elemento a ser eliminado (lembrando que índice começa a ser contado do 0)
+a segunda informação é a QUANTIDADE de itens que queremos eliminar
+então se eu quero eliminar apenas o quinto elemento, eu faço assim:
+
+array.splice(4, 1)
+ou seja, 4 é o índice
+e eu quero eliminar só um elemento, então eu boto o 1*/
+
+//adicionando itens com splice
+//lista3Copia.splice(1,0,'rede');
+//console.log(lista3Copia);
 
